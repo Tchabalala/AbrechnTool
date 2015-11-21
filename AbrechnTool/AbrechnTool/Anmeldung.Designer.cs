8cee5f2820423b1e59d9fad875a239f9e7aea525
Axel <axfr1980@gmail.com>
2015-11-21 13:09:14 +0100

    Neuer User und Anmeldung

---
+++ AbrechnTool/AbrechnTool/Anmeldung.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace AbrechnTool
+{
+    partial class Anmeldung
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_Username = new System.Windows.Forms.TextBox();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.btNewUser = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_Username
+            // 
+            this.tb_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Username.Location = new System.Drawing.Point(12, 12);
+            this.tb_Username.Name = "tb_Username";
+            this.tb_Username.Size = new System.Drawing.Size(170, 29);
+            this.tb_Username.TabIndex = 0;
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Password.Location = new System.Drawing.Point(234, 12);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.PasswordChar = '*';
+            this.tb_Password.Size = new System.Drawing.Size(170, 29);
+            this.tb_Password.TabIndex = 1;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(24, 44);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(145, 24);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Benutzername";
+            this.lblUsername.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(272, 44);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(93, 24);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Passwort";
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btLogin
+            // 
+            this.btLogin.BackColor = System.Drawing.SystemColors.Control;
+            this.btLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLogin.Location = new System.Drawing.Point(28, 74);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(141, 62);
+            this.btLogin.TabIndex = 4;
+            this.btLogin.Text = "Anmelden";
+            this.btLogin.UseVisualStyleBackColor = false;
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // btNewUser
+            // 
+            this.btNewUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btNewUser.Location = new System.Drawing.Point(244, 74);
+            this.btNewUser.Name = "btNewUser";
+            this.btNewUser.Size = new System.Drawing.Size(149, 62);
+            this.btNewUser.TabIndex = 5;
+            this.btNewUser.Text = "Neuer Benutzer";
+            this.btNewUser.UseVisualStyleBackColor = true;
+            this.btNewUser.Click += new System.EventHandler(this.btNewUser_Click);
+            // 
+            // btCancel
+            // 
+            this.btCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCancel.Location = new System.Drawing.Point(133, 142);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(149, 62);
+            this.btCancel.TabIndex = 6;
+            this.btCancel.Text = "Abbrechen";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // Anmeldung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btCancel;
+            this.ClientSize = new System.Drawing.Size(416, 216);
+            this.ControlBox = false;
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btNewUser);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.tb_Username);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Anmeldung";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_Username;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.Button btNewUser;
+        private System.Windows.Forms.Button btCancel;
+    }
+}
+
---
+++ AbrechnTool/AbrechnTool/Anmeldung.cs
@@ -0,0 +1,135 @@
+﻿using AllgLib;
+using SqlLib;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace AbrechnTool
+{
+    public partial class Anmeldung : Form
+    {
+        #region Klassenvariable(n)
+        protected DataSet _dsRightsGroups = null;
+        #endregion
+        #region Eigenschaft(en)
+        #endregion
+        #region Konstruktor(en)
+        public Anmeldung()
+        {
+            InitializeComponent();
+
+            MySqlConnection.Server = Properties.Settings.Default["Server"].ToString();
+            MySqlConnection.Database = Properties.Settings.Default["Database"].ToString();
+            MySqlConnection.Connect();
+
+            this._dsRightsGroups = MySqlConnection.ExecuteStoredProcedureInDataSet("Get_RightsGroups", null);
+        }
+        #endregion
+
+        #region btCancel_Click
+        private void btCancel_Click(object sender, System.EventArgs e)
+        {
+            MySqlConnection.Disconnect();
+
+            this.Close();
+        }
+        #endregion
+
+        #region btNewUser_Click
+        private void btNewUser_Click(object sender, System.EventArgs e)
+        {
+            string cUsername = this.tb_Username.Text;
+            string cPassword = this.tb_Password.Text;
+
+            string cUserEnc = DeEncoding.Encode(cUsername);
+            string cPassEnc = DeEncoding.Encode(cPassword);
+
+            string cProc = "Append_User";
+            List<SqlParameter> aParam = new List<SqlParameter>();
+            SqlParameter param = new SqlParameter("@rightsgroupid", SqlDbType.Int);
+            param.Value = this.GetRightsGroupID();
+            aParam.Add(param);
+
+            param = new SqlParameter("@loginname", SqlDbType.NVarChar);
+            param.Value = cUserEnc;
+            aParam.Add(param);
+
+            param = new SqlParameter("@loginpassword", SqlDbType.NVarChar);
+            param.Value = cPassEnc;
+            aParam.Add(param);
+
+            MySqlConnection.ExecuteStoredProcedure(cProc, aParam);
+        }
+        #endregion
+
+        #region btLogin_Click
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            DataSet userdaten = this.GetUserDaten();
+
+            if (userdaten == null || userdaten.Tables.Count == 0)
+            {
+                MessageBox.Show("Benutzer nicht gefunden!", "Anmeldung fehlgeschlagen");
+            }
+            else
+            {
+            }
+        }
+        #endregion
+
+        #region GetRightsGroupID
+        protected int GetRightsGroupID()
+        {
+            int ret = 0;
+
+            if (this._dsRightsGroups.Tables.Count >= 1)
+            {
+                DataTable dtRightsGroups = this._dsRightsGroups.Tables[0];
+
+                if (dtRightsGroups.Rows.Count >= 1 && dtRightsGroups.Columns.Contains("rightsgroupid")) {
+                    ret = Convert.ToInt32(dtRightsGroups.Rows[0]["rightsgroupid"]);
+                }
+            }
+
+            return ret;
+        }
+        #endregion
+
+        #region GetUserDaten
+        protected DataSet GetUserDaten()
+        {
+            DataSet ret = null;
+
+            try
+            {
+                string cProc = "Get_UserDaten";
+
+                string cUsername = this.tb_Username.Text;
+                string cPassword = this.tb_Password.Text;
+
+                string cUserEnc = DeEncoding.Encode(cUsername);
+                string cPassEnc = DeEncoding.Encode(cPassword);
+
+                List<SqlParameter> aParam = new List<SqlParameter>();
+                SqlParameter param = new SqlParameter("@loginname", SqlDbType.NVarChar);
+                param.Value = cUserEnc;
+                aParam.Add(param);
+
+                param = new SqlParameter("@loginpassword", SqlDbType.NVarChar);
+                param.Value = cPassEnc;
+                aParam.Add(param);
+
+                ret = MySqlConnection.ExecuteStoredProcedureInDataSet(cProc, aParam);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Fehler beim Abruf der Userdaten");
+            }
+
+            return ret;
+        }
+        #endregion
+    }
+}
